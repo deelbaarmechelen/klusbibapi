3f8d0d77f90c4d22d80690c13330e809b5042058
Bernard Butaye <bernardenmartine@gmail.com>
2017-10-31 00:06:26 +0100

    switch travis to test database url

--- phinx.php
+++ phinx.php
@@ -29,22 +29,22 @@ $usertst = $dsntst->user;
 $passtst = $dsntst->pass;
 $porttst = $dsntst->port;
 
-require_once __DIR__ . '/src/env.php';
-if (empty($host)) {
-	$host = "127.0.0.1";
-}
-if (empty($user)) {
-	$user = "root";
-}
-if (empty($pass)) {
-	$pass = "";
-}
-if (empty($database)) {
-	$database = "klusbibapi_test";
-}
-if (empty($port)) {
-	$port = "3306";
-}
+require __DIR__ . '/tests/test_env.php';
+// if (empty($host)) {
+// 	$host = "127.0.0.1";
+// }
+// if (empty($user)) {
+// 	$user = "root";
+// }
+// if (empty($pass)) {
+// 	$pass = "";
+// }
+// if (empty($database)) {
+// 	$database = "klusbibapi_test";
+// }
+// if (empty($port)) {
+// 	$port = "3306";
+// }
 
 
 $settings = require __DIR__ . '/src/settings.php';
