5cb5cbecf5821fbcd11be0d208dea094f83f5fdf
Bernard Butaye <bernardenmartine@gmail.com>
2017-10-30 23:26:40 +0100

    workaround to missing host, user, pass database and port values for travis

--- phinx.php
+++ phinx.php
@@ -30,6 +30,22 @@ $passtst = $dsntst->pass;
 $porttst = $dsntst->port;
 
 require_once __DIR__ . '/src/env.php';
+if (empty($host)) {
+	$host = "127.0.0.1";
+}
+if (empty($user)) {
+	$user = "root";
+}
+if (empty($pass)) {
+	$pass = "";
+}
+if (empty($database)) {
+	$database = "klusbibapi_test";
+}
+if (empty($port)) {
+	$port = "3306";
+}
+
 
 $settings = require __DIR__ . '/src/settings.php';
 
