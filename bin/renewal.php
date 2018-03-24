6b8fd8eda351a525c064b104531cb6ad90123b1a
Bernard Butaye <bernardenmartine@gmail.com>
2018-03-25 00:47:20 +0100

    forcing PHP 7.0.x to avoid compatibilty issues with illuminate 5.2.x

--- bin/renewal.php
+++ bin/renewal.php
@@ -18,14 +18,16 @@ $mailmgr->sendRenewal($user);
 
 $fromDate = date('Y-m-d' . ' 00:00:00', strtotime("+1 week"));
 $toDate   = date('Y-m-d' . ' 23:59:59', strtotime("+1 week"));
+echo "selecting active users between $fromDate and $toDate";
 $users = \Api\Model\User::active()->whereBetween('membership_end_date' , [$fromDate, $toDate])->get();
+echo "selected users: " . $users.count();
 // TODO: log renewal events
 foreach ($users as $user) {
     echo "renewal required for user $user->user_id\n";
-    echo "name: " . $user->firstname . " " . $user->lastname;
-    echo "state: " . $user->state;
-    echo "membership start: " . $user->membership_start_date;
-    echo "membership end: " . $user->membership_end_date;
+    echo "name: " . $user->firstname . " " . $user->lastname . "\n";
+    echo "state: " . $user->state . "\n";
+    echo "membership start: " . $user->membership_start_date . "\n";
+    echo "membership end: " . $user->membership_end_date . "\n";
 }
 echo "end of renewal cron";
 
