0f86eb4f284b9ea93462762dbb67dcb4b64cacec
Bernard Butaye <bernardenmartine@gmail.com>
2018-03-25 22:11:48 +0200

    enable renewal emails

--- bin/renewal.php
+++ bin/renewal.php
@@ -6,21 +6,14 @@ if (isset($_SERVER['REMOTE_ADDR'])) die('Permission denied.');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/env.php';
 $settings = require __DIR__ . '/../src/settings.php';
-echo "test renewal cron";
+echo "Start renewal cron\n";
 $mailmgr = new \Api\Mail\MailManager();
-$user = new UserTest();
-$user->email = "bernard@klusbib.be";
-$user->firstname = "tester";
-$user->lastname = "de mock";
-$user->membership_end_date = date('Y-m-d');
-
-$mailmgr->sendRenewal($user);
 
 $fromDate = date('Y-m-d' . ' 00:00:00', strtotime("+1 week"));
 $toDate   = date('Y-m-d' . ' 23:59:59', strtotime("+1 week"));
-echo "selecting active users between $fromDate and $toDate";
+echo "selecting active users between $fromDate and $toDate \n";
 $users = \Api\Model\User::active()->whereBetween('membership_end_date' , [$fromDate, $toDate])->get();
-echo "selected users: " . $users.count();
+echo "selected users: " . count($users) . "\n";
 // TODO: log renewal events
 foreach ($users as $user) {
     echo "renewal required for user $user->user_id\n";
@@ -28,18 +21,7 @@ foreach ($users as $user) {
     echo "state: " . $user->state . "\n";
     echo "membership start: " . $user->membership_start_date . "\n";
     echo "membership end: " . $user->membership_end_date . "\n";
+    echo "email: " . $user->email . "\n";
+    $mailmgr->sendRenewal($user);
 }
-echo "end of renewal cron";
-
-
-class UserTest {
-    //['user_id', 'state', 'firstname', 'lastname', 'role', 'email',
-    //'membership_start_date', 'membership_end_date', 'birth_date', 'address', 'postal_code', 'city',
-    //'phone', 'mobile', 'registration_number', 'payment_mode', 'created_at', 'updated_at'
-    public $firstname;
-    public $lastname;
-    public $role;
-    public $email;
-    public $membership_start_date;
-    public $membership_end_date;
-}
+echo "End of renewal cron\n";
