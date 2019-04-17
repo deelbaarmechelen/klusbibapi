87f87ff37b7075c4a5ed38376582134484be1ef6
Bernard Butaye <bernardenmartine@gmail.com>
2019-04-17 16:12:58 +0200

    delete users expired for more than 1 year

--- bin/expiry.php
+++ bin/expiry.php
@@ -2,6 +2,23 @@
 <?php
 # Deny access from the web
 if (isset($_SERVER['REMOTE_ADDR'])) die('Permission denied.');
+//
+//if (isset($argc)) {
+//    for ($i = 0; $i < $argc; $i++) {
+//        echo "Argument #" . $i . " - " . $argv[$i] . "\n";
+//    }
+//}
+//else {
+//    echo "argc and argv disabled\n";
+//}
+
+$val = getopt("d", ["delete"]);
+
+$delete = false;
+if (isset($val["delete"]) || isset($val["d"])) {
+    echo "Effective delete enabled\n";
+    $delete = true;
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/env.php';
@@ -23,8 +40,40 @@ foreach ($users as $user) {
     $user->state = 'EXPIRED';
     $user->save();
 }
+
+// Delete users when membership expired for more than 1 year
+$lastYear = date('Y-m-d' . ' 00:00:00', strtotime("-1 year"));
+echo "selecting expired users for more than 1 year (on $lastYear)\n";
+$users = \Api\Model\User::expired()->notAdmin()->whereDate('membership_end_date' , '<', $lastYear)->get();
+echo "selected users: " . count($users) . "\n";
+
+foreach ($users as $user) {
+    echo "Removal required for user $user->user_id\n";
+    echo "name: " . $user->firstname . " " . $user->lastname . "\n";
+    echo "state: " . $user->state . "\n";
+    echo "membership start: " . $user->membership_start_date . "\n";
+    echo "membership end: " . $user->membership_end_date . "\n";
+    echo "email: " . $user->email . "\n";
+    $user->state = 'DELETED';
+    $user->save();
+}
 $activeCount = \Api\Model\User::active()->members()->count();
 $expiredCount = \Api\Model\User::where('state', 'EXPIRED')->count();
+$deletedCount = \Api\Model\User::where('state', 'DELETED')->count();
 echo "Active users: $activeCount\n";
 echo "Expired users: $expiredCount\n";
+echo "Deleted users: $deletedCount\n";
+
+if ($delete) {
+    $usersToDelete = \Api\Model\User::where('state', '=', \Api\Model\UserState::DELETED)->get();
+    foreach ($usersToDelete as $user) {
+        echo "Real delete for user $user->user_id\n";
+        echo "name: " . $user->firstname . " " . $user->lastname . "\n";
+        echo "state: " . $user->state . "\n";
+        echo "membership start: " . $user->membership_start_date . "\n";
+        echo "membership end: " . $user->membership_end_date . "\n";
+        echo "email: " . $user->email . "\n";
+        $user->delete();
+    }
+}
 echo "End of expiry cron\n";
