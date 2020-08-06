4ccd26dc9e37bf72f63023248757f5479cbfea58
Bernard Butaye <bernard@deelbaarmechelen.be>
2020-08-06 16:07:08 +0200

    local version of abandoned dbunit and There4\Slim\Test libraries

---
+++ thirdParty/There4/Slim/Test/WebDbTestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace There4\Slim\Test;
+
+use PDO;
+use PHPUnit\DbUnit\TestCase;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+
+class WebDbTestCase extends TestCase
+{
+    /** @var \Slim\App */
+    protected $app;
+
+    /** @var WebTestClient */
+    public $client;
+
+    /** Database Connection **/
+    protected $conn;
+
+    // Run for each unit test to setup our slim app environment
+    public function setup() : void
+    {
+        parent::setUp();
+
+        // Establish a local reference to the Slim app object
+        $this->app    = $this->getSlimInstance();
+        $this->client = new WebTestClient($this->app);
+    }
+
+    // Instantiate a Slim application for use in our testing environment. You
+    // will most likely override this for your own application.
+    public function getSlimInstance()
+    {
+        return SlimInstance::getInstance();
+    }
+
+    public function getConnection()
+    {
+        if ($this->conn === null) {
+            $pdo = new PDO('sqlite::memory:');
+            $this->conn = $this->createDefaultDBConnection($pdo, ':memory:');
+        }
+        return $this->conn;
+    }
+
+    public function getDataSet()
+    {
+        return new QueryDataSet(
+            $this->getConnection()
+        );
+    }
+}
---
+++ thirdParty/There4/Slim/Test/WebTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace There4\Slim\Test;
+
+class WebTestCase extends \PHPUnit\Framework\TestCase
+{
+    /** @var \Slim\App */
+    protected $app;
+
+    /** @var WebTestClient */
+    protected $client;
+
+    // Run for each unit test to setup our slim app environment
+    public function setup() : void
+    {
+        // Establish a local reference to the Slim app object
+        // Ensure no cache Router
+        $this->app    = $this->getSlimInstance();
+        $this->client = new WebTestClient($this->app);
+    }
+
+    // Return the configured Slim App object
+    public function getSlimInstance()
+    {
+        return SlimInstance::getInstance();
+    }
+}
