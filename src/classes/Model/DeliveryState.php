59b3f7f5fc84c781141a5cd52a3b097f9b2c38ec
Bernard Butaye <bernard@deelbaarmechelen.be>
2020-10-25 21:14:14 +0100

    Fixes in deliveries

--- src/classes/Model/DeliveryState.php
+++ src/classes/Model/DeliveryState.php
@@ -4,6 +4,7 @@ namespace Api\Model;
 abstract class DeliveryState
 {
 	const REQUESTED = 'REQUESTED';
+    const CONFIRMED = 'CONFIRMED';
 	const DELIVERED = 'DELIVERED';
 	const CANCELLED = 'CANCELLED';
 }
