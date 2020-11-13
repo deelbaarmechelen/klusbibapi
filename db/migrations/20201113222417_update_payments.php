acfd0e9a5c34893c334442224b3b739de40cce93
Bernard Butaye <bernard@deelbaarmechelen.be>
2020-11-13 23:37:41 +0100

    update payments table to store vouchers

---
+++ db/migrations/20201113222417_update_payments.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require_once __DIR__ . '/../../src/env.php';
+require_once __DIR__ . '/../../src/settings.php';
+/**
+ * Custom template for database migration with Illuminate\Database
+ * 
+ * Default template can be found at https://github.com/robmorgan/phinx/blob/master/src/Phinx/Migration/Migration.template.php.dist
+ */
+class UpdatePayments extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * Called when invoking migrate
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+	public function up()
+	{
+        Capsule::schema()->table('payments', function(Illuminate\Database\Schema\Blueprint $table){
+            $table->string('payment_ext_id', 20)->nullable()->default(null);
+            $table->timestamp('expiration_date')->nullable()->default(null);
+        });
+	}
+    /**
+     * Down Method.
+     *
+     * Called when invoking rollback
+     */
+	public function down()
+	{
+        Capsule::schema()->table('payments', function(Illuminate\Database\Schema\Blueprint $table){
+            $table->dropColumn('payment_ext_id');
+            $table->dropColumn('expiration_date');
+        });
+	}
+}
