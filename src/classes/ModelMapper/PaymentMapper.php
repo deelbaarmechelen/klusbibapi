83fcb9916898d4f201f0c9499743cfea77695417
Bernard Butaye <bernard@deelbaarmechelen.be>
2021-02-20 18:02:47 +0100

    add new payment fields to API

--- src/classes/ModelMapper/PaymentMapper.php
+++ src/classes/ModelMapper/PaymentMapper.php
@@ -1,22 +1,25 @@
 <?php
 namespace Api\ModelMapper;
 
-use \Api\Model\User;
-
 class PaymentMapper
 {
 	static public function mapPaymentToArray($payment) {
 	
 		$paymentArray = array("payment_id" => $payment->payment_id,
-				"user_id" => $payment->user_id,
-				"state" => $payment->state,
-				"mode" => $payment->mode,
-				"payment_date" => $payment->payment_date,
-				"order_id" => $payment->order_id,
-				"amount" => $payment->amount,
-				"currency" => $payment->currency,
-				"created_at" => $payment->created_at,
-				"updated_at" => $payment->updated_at,
+            "user_id" => $payment->user_id,
+            "state" => $payment->state,
+            "mode" => $payment->mode,
+            "payment_date" => $payment->payment_date,
+            "order_id" => $payment->order_id,
+            "amount" => $payment->amount,
+            "currency" => $payment->currency,
+            "comment" => $payment->comment,
+            "payment_ext_id" => $payment->payment_ext_id,
+            "expiration_date" => $payment->expiration_date,
+            "membership_id" => $payment->membership_id,
+            "loan_id" => $payment->loan_id,
+            "created_at" => $payment->created_at,
+            "updated_at" => $payment->updated_at,
 		);
 		return $paymentArray;
 	}
@@ -44,6 +47,21 @@ class PaymentMapper
         }
         if (isset($data["currency"]) && $isAdmin) {
             $payment->currency = $data["currency"];
+        }
+        if (isset($data["comment"]) && $isAdmin) {
+            $payment->comment = $data["comment"];
+        }
+        if (isset($data["payment_ext_id"]) && $isAdmin) {
+            $payment->payment_ext_id = $data["payment_ext_id"];
+        }
+        if (isset($data["expiration_date"]) && $isAdmin) {
+            $payment->expiration_date = $data["expiration_date"];
+        }
+        if (isset($data["membership_id"]) && $isAdmin) {
+            $payment->membership_id = $data["membership_id"];
+        }
+        if (isset($data["loan_id"]) && $isAdmin) {
+            $payment->loan_id = $data["loan_id"];
         }
 	}
 }
