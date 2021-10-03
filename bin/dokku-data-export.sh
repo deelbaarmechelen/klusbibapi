9679747b2250baadd780679d024a609bb522f0e0
Bernard Butaye <bernard@deelbaarmechelen.be>
2021-10-03 15:37:36 +0200

    update cron jobs incl backup of lendengine

--- bin/dokku-data-export.sh
+++ bin/dokku-data-export.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
-echo "Backing up data to Dropbox ..."
+LOGFILE=/var/log/dokku/backup_data.log
+echo "Backing up data from Klusbib API..." >> $LOGFILE
 
 dt=$(date +"%Y-%m-%d")
 
-echo " today is $dt"
+echo " today is $dt" >> $LOGFILE
 
-DATA_PATH=/var/lib/dokku/data
-BACKUP_PATH=/home/dokku/Dropbox/backup/dokku/$(date +"%Y")/$(date +"%B")
+#DATA_PATH=/var/lib/dokku/data
+DATA_PATH=/var/lib/dokku/data/storage
+#BACKUP_PATH=/home/klusbib/Dropbox/backup/dokku/$(date +"%Y")/$(date +"%B")
+BACKUP_PATH=remote:dokku/data/storage/$(date +"%Y")
 
-echo " creating $BACKUP_PATH .."
-mkdir -p $BACKUP_PATH
+#echo " creating $BACKUP_PATH .." >> $LOGFILE
+#mkdir -p $BACKUP_PATH
 
-cp -pr $DATA_PATH $BACKUP_PATH
+#cp -pr $DATA_PATH $BACKUP_PATH
+rclone sync $DATA_PATH/klusbibapi $BACKUP_PATH/klusbibapi -P >> $LOGFILE
+rclone sync $DATA_PATH/inventory $BACKUP_PATH/inventory -P >> $LOGFILE
+rclone sync $DATA_PATH/lendengine $BACKUP_PATH/lendengine -P >> $LOGFILE
+echo " backup of $DATA_PATH to $BACKUP_PATH completed" >> $LOGFILE
--- bin/dokku-mysql-backup.sh
+++ bin/dokku-mysql-backup.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
+LOGFILE=/var/log/dokku/backup_mysql.log
 
-echo "Backing up Mysql databases to Dropbox ..."
+echo "Backing up Mysql databases from Dokku ..." >> $LOGFILE
 
 dt=$(date +"%Y-%m-%d")
 
-echo " today is $dt"
+echo " today is $dt" >> $LOGFILE
 
-BACKUP_PATH=/home/dokku/Dropbox/backup/mysql/$(date +"%Y")/$(date +"%B")
-echo " creating $BACKUP_PATH .."
+BACKUP_PATH=remote:dokku/mysql/$(date +"%Y")/$(date +"%B")
+TEMP_DIR=/tmp/backup
+LENDENGINE_PWD=<pwd>
 
-dbs=$(dokku mysql:list | grep mysql | cut -f1 -d' ')
+echo " creating $TEMP_DIR .." >> $LOGFILE
+mkdir -p $TEMP_DIR >> $LOGFILE
+
+#dbs=$(dokku mysql:list | grep mysql | cut -f1 -d' ')
+dbs=$(dokku mysql:list | grep db )
 
 for db in $dbs
 do
-  echo " backing up $db ..."
-  mkdir -p $BACKUP_PATH/$db
-  f=$BACKUP_PATH/$db/$dt-$db.tar
+  echo " backing up $db ..." >> $LOGFILE
+  mkdir -p $TEMP_DIR/$db >> $LOGFILE
+  f=$TEMP_DIR/$db/$dt-$db.sql
   rm -f $f
   dokku mysql:export $db > $f
   gzip -f $f
+  echo " backup file created at $f.gz" >> $LOGFILE
+  rclone copy $f.gz $BACKUP_PATH/$db >> $LOGFILE
+  rm -f $f.gz
+  echo " backup file $f.gz transferred to $BACKUP_PATH/$db" >> $LOGFILE
 done
+echo " Lend Engine backup" >> $LOGFILE
+rm -f $TEMP_DIR/klusbibdb/*.sql
+sudo docker exec dokku.mysql.klusbibdb mysqldump --add-drop-table -p$LENDENGINE_PWD _core > $TEMP_DIR/klusbibdb/_core.sql
+sudo docker exec dokku.mysql.klusbibdb mysqldump --add-drop-table -p$LENDENGINE_PWD lendengine > $TEMP_DIR/klusbibdb/lendengine.sql
+f=$TEMP_DIR/klusbibdb/$dt-klusbibdb-lendengine.tar.gz
+rm -f $f
+cd $TEMP_DIR/klusbibdb
+tar -czf $f *.sql
+echo " backup file created at $f" >> $LOGFILE
+rclone copy $f $BACKUP_PATH/klusbibdb >> $LOGFILE
+rm -f $f
+echo " backup file $f transferred to $BACKUP_PATH/klusbibdb" >> $LOGFILE
 
+echo "Mysql backup completed" >> $LOGFILE
