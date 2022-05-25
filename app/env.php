4c12fa58b8fa6d82b81a42c246f84e77b27ab156
Bernard Butaye <bernard@deelbaarmechelen.be>
2022-05-25 22:51:07 +0200

    replace getenv calls by access to  super variable

--- app/env.php
+++ app/env.php
@@ -4,7 +4,7 @@ if (file_exists(__DIR__ . '/../.env')) {
 	$dotenv->load();
 }
 
-$url = getenv('DATABASE_URL');
+$url = $_ENV['DATABASE_URL'];
 if (isset($url) && !empty($url)) {
     $host = parse_url($url, PHP_URL_HOST);
     $database = substr(parse_url($url, PHP_URL_PATH), 1);
@@ -13,51 +13,51 @@ if (isset($url) && !empty($url)) {
     $port = parse_url($url, PHP_URL_PORT);
 }
 else {
-	$host = getenv('DB_HOST');
-	$database = getenv('DB_NAME');
-	$user = getenv('DB_USER');
-	$pass = getenv('DB_PASS');
-	$port = getenv('DB_PORT');
+	$host = $_ENV['DB_HOST'] ;
+	$database = $_ENV['DB_NAME'] ;
+	$user = $_ENV['DB_USER'] ;
+	$pass = $_ENV['DB_PASS'] ;
+	$port = $_ENV['DB_PORT'] ;
 }
 
 if (!defined('APP_ENV')) {
-    $appEnv = getenv('APP_ENV');
+    $appEnv = $_ENV['APP_ENV'];
     define('APP_ENV',(isset($appEnv) ? $appEnv : 'production') ); // defaults to production
 }
-if (!defined('PROJECT_HOME')) define("PROJECT_HOME",getenv('PROJECT_HOME'));
+if (!defined('PROJECT_HOME')) define("PROJECT_HOME", $_ENV['PROJECT_HOME']);
 
-if (!defined('MAIL_PORT')) define("MAIL_PORT", getenv('MAIL_PORT')); // smtp port number
-if (!defined('MAIL_USERNAME')) define("MAIL_USERNAME", getenv('MAIL_USERNAME')); // smtp username
-if (!defined('MAIL_PASSWORD')) define("MAIL_PASSWORD", getenv('MAIL_PASSWORD')); // smtp password
-if (!defined('MAIL_HOST')) define("MAIL_HOST", getenv('MAIL_HOST')); // smtp host
+if (!defined('MAIL_PORT')) define("MAIL_PORT", $_ENV['MAIL_PORT']); // smtp port number
+if (!defined('MAIL_USERNAME')) define("MAIL_USERNAME", $_ENV['MAIL_USERNAME'] ); // smtp username
+if (!defined('MAIL_PASSWORD')) define("MAIL_PASSWORD", $_ENV['MAIL_PASSWORD'] ); // smtp password
+if (!defined('MAIL_HOST')) define("MAIL_HOST", $_ENV['MAIL_HOST']); // smtp host
 // smtp auth type
 if (!defined('MAIL_AUTH_TYPE')) {
-    $authType = getenv('MAIL_AUTH_TYPE');
+    $authType = $_ENV['MAIL_AUTH_TYPE'] ;
     define("MAIL_AUTH_TYPE", $authType != false ? $authType : 'XOAUTH2');
 }
-if (!defined('MAILER')) define("MAILER", getenv('MAILER'));
-if (!defined('OAUTH_CLIENT_ID')) define("OAUTH_CLIENT_ID", getenv('OAUTH_CLIENT_ID'));
-if (!defined('OAUTH_CLIENT_SECRET')) define("OAUTH_CLIENT_SECRET", getenv('OAUTH_CLIENT_SECRET'));
-if (!defined('OAUTH_TOKEN')) define("OAUTH_TOKEN", getenv('OAUTH_TOKEN'));
+if (!defined('MAILER')) define("MAILER", $_ENV['MAILER'] );
+if (!defined('OAUTH_CLIENT_ID')) define("OAUTH_CLIENT_ID", $_ENV['OAUTH_CLIENT_ID'] );
+if (!defined('OAUTH_CLIENT_SECRET')) define("OAUTH_CLIENT_SECRET", $_ENV['OAUTH_CLIENT_SECRET'] );
+if (!defined('OAUTH_TOKEN')) define("OAUTH_TOKEN", $_ENV['OAUTH_TOKEN'] );
 
-if (!defined('SENDER_NAME')) define("SENDER_NAME", getenv('SENDER_NAME'));
-if (!defined('SENDER_EMAIL')) define("SENDER_EMAIL", getenv('SENDER_EMAIL'));
-if (!defined('REPLYTO_NAME')) define("REPLYTO_NAME", getenv('REPLYTO_NAME'));
-if (!defined('REPLYTO_EMAIL')) define("REPLYTO_EMAIL", getenv('REPLYTO_EMAIL'));
-if (!defined('SUPPORT_NOTIF_EMAIL')) define("SUPPORT_NOTIF_EMAIL", getenv('SUPPORT_NOTIF_EMAIL'));
-if (!defined('ENROLMENT_NOTIF_EMAIL')) define("ENROLMENT_NOTIF_EMAIL", getenv('ENROLMENT_NOTIF_EMAIL'));
-if (!defined('RESERVATION_NOTIF_EMAIL')) define("RESERVATION_NOTIF_EMAIL", getenv('RESERVATION_NOTIF_EMAIL'));
-if (!defined('DELIVERY_NOTIF_EMAIL')) define("DELIVERY_NOTIF_EMAIL", getenv('DELIVERY_NOTIF_EMAIL'));
-if (!defined('STROOM_NOTIF_EMAIL')) define("STROOM_NOTIF_EMAIL", getenv('STROOM_NOTIF_EMAIL'));
+if (!defined('SENDER_NAME')) define("SENDER_NAME", $_ENV['SENDER_NAME'] );
+if (!defined('SENDER_EMAIL')) define("SENDER_EMAIL", $_ENV['SENDER_EMAIL'] );
+if (!defined('REPLYTO_NAME')) define("REPLYTO_NAME", $_ENV['REPLYTO_NAME'] );
+if (!defined('REPLYTO_EMAIL')) define("REPLYTO_EMAIL", $_ENV['REPLYTO_EMAIL'] );
+if (!defined('SUPPORT_NOTIF_EMAIL')) define("SUPPORT_NOTIF_EMAIL", $_ENV['SUPPORT_NOTIF_EMAIL'] );
+if (!defined('ENROLMENT_NOTIF_EMAIL')) define("ENROLMENT_NOTIF_EMAIL", $_ENV['ENROLMENT_NOTIF_EMAIL'] );
+if (!defined('RESERVATION_NOTIF_EMAIL')) define("RESERVATION_NOTIF_EMAIL", $_ENV['RESERVATION_NOTIF_EMAIL'] );
+if (!defined('DELIVERY_NOTIF_EMAIL')) define("DELIVERY_NOTIF_EMAIL", $_ENV['DELIVERY_NOTIF_EMAIL'] );
+if (!defined('STROOM_NOTIF_EMAIL')) define("STROOM_NOTIF_EMAIL", $_ENV['STROOM_NOTIF_EMAIL'] );
 
-if (!defined('MOLLIE_API_KEY')) define("MOLLIE_API_KEY", getenv('MOLLIE_API_KEY'));
-if (!defined('INVENTORY_API_KEY')) define("INVENTORY_API_KEY", getenv('INVENTORY_API_KEY'));
-if (!defined('INVENTORY_URL')) define("INVENTORY_URL", getenv('INVENTORY_URL'));
+if (!defined('MOLLIE_API_KEY')) define("MOLLIE_API_KEY", $_ENV['MOLLIE_API_KEY'] );
+if (!defined('INVENTORY_API_KEY')) define("INVENTORY_API_KEY", $_ENV['INVENTORY_API_KEY'] );
+if (!defined('INVENTORY_URL')) define("INVENTORY_URL", $_ENV['INVENTORY_URL'] );
 if (!defined('WEB_URL')) {
-    $webUrl = getenv('WEB_URL');
+    $webUrl = $_ENV['WEB_URL'] ;
     define("WEB_URL", $webUrl != false ? $webUrl : 'https://www.klusbib.be');
 }
 if (!defined('LAST_TERMS_DATE')) {
-    $lastTermsDate = getenv('LAST_TERMS_DATE');
+    $lastTermsDate = $_ENV['LAST_TERMS_DATE'] ;
     define("LAST_TERMS_DATE", $lastTermsDate != false ? $lastTermsDate : '2019-12-02');
 }
