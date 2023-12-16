e653c8775e767282e2a6d04df22f5bf1934d05a0
Bernard Butaye <renardeau@users.noreply.github.com>
2023-12-16 18:04:11 +0100

    sync inventory assets with lendengine inventory_item

--- db/migrations/20231216103459_create_sync_assets.php
+++ db/migrations/20231216103459_create_sync_assets.php
@@ -58,61 +58,63 @@ class CreateSyncAssets extends AbstractCapsuleMigration
 
         $db = Capsule::Connection()->getPdo();
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, 0);
-        $sql = "
-        CREATE TRIGGER inventory.`assets_ai` AFTER INSERT ON inventory.`assets` FOR EACH ROW INSERT INTO klusbibdb.kb_sync_assets (
-            id, name, asset_tag, model_id, image, status_id, assigned_to, assigned_type, last_checkout, last_checkin, expected_checkin, created_at, updated_at, deleted_at)
-           VALUES (
-            NEW.id, NEW.name, NEW.asset_tag, NEW.model_id, NEW.image, NEW.status_id, NEW.assigned_to, NEW.assigned_type, NEW.last_checkout, NEW.last_checkin, NEW.expected_checkin, NEW.created_at, NEW.updated_at, NEW.deleted_at)";
-        $db->exec($sql);
+//         $sql = "
+//         CREATE TRIGGER inventory.`assets_ai` AFTER INSERT ON inventory.`assets` FOR EACH ROW INSERT INTO klusbibdb.kb_sync_assets (
+//             id, name, asset_tag, model_id, image, status_id, assigned_to, assigned_type, last_checkout, last_checkin, expected_checkin, created_at, updated_at, deleted_at)
+//            VALUES (
+//             NEW.id, NEW.name, NEW.asset_tag, NEW.model_id, NEW.image, NEW.status_id, NEW.assigned_to, NEW.assigned_type, NEW.last_checkout, NEW.last_checkin, NEW.expected_checkin, NEW.created_at, NEW.updated_at, NEW.deleted_at)";
+//         $db->exec($sql);
 
-        $sql = "
-        CREATE TRIGGER inventory.`assets_au` AFTER UPDATE ON inventory.`assets` FOR EACH ROW
-         UPDATE klusbibdb.kb_sync_assets 
-         SET name = NEW.name,
-          asset_tag = NEW.asset_tag,
-          model_id = NEW.model_id,
-          image = NEW.image,
-          status_id = NEW.status_id,
-          assigned_to = NEW.assigned_to,
-          assigned_type = NEW.assigned_type, 
-          last_checkout = NEW.last_checkout,
-          last_checkin = NEW.last_checkin, 
-          expected_checkin = NEW.expected_checkin, 
-          created_at = NEW.created_at, 
-          updated_at = NEW.updated_at, 
-          deleted_at = NEW.deleted_at 
-          WHERE id = NEW.id;";
-        $db->exec($sql);
+//         $sql = "
+//         CREATE TRIGGER inventory.`assets_au` AFTER UPDATE ON inventory.`assets` FOR EACH ROW
+//          UPDATE klusbibdb.kb_sync_assets 
+//          SET name = NEW.name,
+//           asset_tag = NEW.asset_tag,
+//           model_id = NEW.model_id,
+//           image = NEW.image,
+//           status_id = NEW.status_id,
+//           assigned_to = NEW.assigned_to,
+//           assigned_type = NEW.assigned_type, 
+//           last_checkout = NEW.last_checkout,
+//           last_checkin = NEW.last_checkin, 
+//           expected_checkin = NEW.expected_checkin, 
+//           created_at = NEW.created_at, 
+//           updated_at = NEW.updated_at, 
+//           deleted_at = NEW.deleted_at 
+//           WHERE id = NEW.id;";
+//         $db->exec($sql);
 
-        $sql = "CREATE TRIGGER inventory.`assets_ad` AFTER DELETE ON inventory.`assets` FOR EACH ROW DELETE FROM klusbibdb.kb_sync_assets WHERE id = OLD.id;";
-        $db->exec($sql);
+//         $sql = "CREATE TRIGGER inventory.`assets_ad` AFTER DELETE ON inventory.`assets` FOR EACH ROW DELETE FROM klusbibdb.kb_sync_assets WHERE id = OLD.id;";
+//         $db->exec($sql);
 
-        // Keep assets/kb_sync_assets and inventory_item in sync
+//         // Keep assets/kb_sync_assets and inventory_item in sync
 
-        // Add kb_sync_assets triggers to update inventory_item        
-        $sql = " 
-CREATE TRIGGER klusbibdb.`kb_sync_assets_bi` BEFORE INSERT ON klusbibdb.`kb_sync_assets` FOR EACH ROW
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM klusbibdb.inventory_item WHERE id = NEW.id) THEN
+//         // Add kb_sync_assets triggers to update inventory_item        
+//         $sql = " 
+// CREATE TRIGGER klusbibdb.`kb_sync_assets_bi` BEFORE INSERT ON klusbibdb.`kb_sync_assets` FOR EACH ROW
+// BEGIN
+//     IF NOT EXISTS (SELECT 1 FROM klusbibdb.inventory_item WHERE id = NEW.id) THEN
 
-        INSERT INTO klusbibdb.inventory_item (
-        id, created_by, assigned_to, current_location_id, item_condition, created_at, updated_at,
-        name, sku, description, keywords, brand, care_information, component_information, 
-        loan_fee, max_loan_days, is_active, show_on_website, serial, note, price_cost, price_sell, short_url, 
-        item_sector, is_reservable, deposit_amount, item_type, donated_by, owned_by)
-        SELECT 
-        NEW.`id`, null, NEW.`assigned_to`, null, null, ifnull(NEW.`created_at`, CURRENT_TIMESTAMP), ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP), 
-        NEW.`name`, NEW.`asset_tag`, null, null, null, null,
-        null, null, 1, 1, null, null, null, null, null, 
-        null, 1, null, 'loan', null, null;
+//         INSERT INTO klusbibdb.inventory_item (
+//         id, created_by, assigned_to, current_location_id, item_condition, created_at, updated_at,
+//         name, sku, description, keywords, brand, care_information, component_information, 
+//         loan_fee, max_loan_days, is_active, show_on_website, serial, note, price_cost, price_sell, short_url, 
+//         item_sector, is_reservable, deposit_amount, item_type, donated_by, owned_by)
+//         SELECT 
+//         NEW.`id`, null, NEW.`assigned_to`, null, null, ifnull(NEW.`created_at`, CURRENT_TIMESTAMP), ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP), 
+//         NEW.`name`, NEW.`asset_tag`, null, null, null, null,
+//         null, null, 1, 1, null, null, null, null, null, 
+//         null, 1, null, 'loan', null, null;
 
-    END IF;
-END
-";
-        $db->exec($sql);
+//     END IF;
+// END
+// ";
+//        $db->exec($sql);
+
         $sql = "
 CREATE TRIGGER klusbibdb.`kb_sync_assets_bu` BEFORE UPDATE ON klusbibdb.`kb_sync_assets` FOR EACH ROW
 BEGIN
+    DECLARE dummy_ INT(11);
     IF EXISTS (SELECT 1 FROM klusbibdb.inventory_item WHERE id = OLD.id) THEN
         IF NOT OLD.name <=> NEW.name THEN
             UPDATE klusbibdb.`inventory_item`
@@ -120,46 +122,47 @@ BEGIN
             updated_at = ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP)
             WHERE id = OLD.id;
         END IF;
-        IF NOT OLD.sku <=> NEW.sku THEN
+        IF NOT OLD.asset_tag <=> NEW.asset_tag THEN
             UPDATE klusbibdb.`inventory_item`
             SET sku = NEW.asset_tag,
             updated_at = ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP)
             WHERE id = OLD.id;
         END IF;
-        IF NOT OLD.assigned_to <=> NEW.assigned_to THEN
-        END IF;
     ELSE
         INSERT INTO klusbibdb.inventory_item (
-        id, created_by, assigned_to, current_location_id, item_condition, created_at, updated_at,
-        name, sku, description, keywords, brand, care_information, component_information, 
-        loan_fee, max_loan_days, is_active, show_on_website, serial, note, price_cost, price_sell, short_url, 
-        item_sector, is_reservable, deposit_amount, item_type, donated_by, owned_by)
+        id, assigned_to, created_at, updated_at,
+        name, sku, item_type)
         SELECT 
-        NEW.`id`, null, NEW.`assigned_to`, null, null, ifnull(NEW.`created_at`, CURRENT_TIMESTAMP), ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP), 
-        NEW.`name`, NEW.`asset_tag`, null, null, null, null,
-        null, null, 1, 1, null, null, null, null, null, 
-        null, 1, null, 'loan', null, null;
+        NEW.`id`, NEW.`assigned_to`, ifnull(NEW.`created_at`, CURRENT_TIMESTAMP), ifnull(NEW.`updated_at`, CURRENT_TIMESTAMP), 
+        NEW.`name`, NEW.`asset_tag`, 'loan';
     END IF;
 
     IF (NOT NEW.model_id <=> OLD.model_id) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
     IF (NOT NEW.image <=> OLD.image) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
-    IF (NOT NEW.status <=> OLD.status) THEN
+    IF (NOT NEW.status_id <=> OLD.status_id) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
     IF (NOT NEW.last_checkout <=> OLD.last_checkout)  THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
     IF (NOT NEW.last_checkin <=> OLD.last_checkin) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
     IF (NOT NEW.expected_checkin <=> OLD.expected_checkin) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
     IF (NOT NEW.assigned_to <=> OLD.assigned_to) THEN
+        SELECT 1 INTO dummy_;
     END IF;
 
 END
