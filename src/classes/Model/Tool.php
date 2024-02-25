ea6cf711bc9f9213e8ee07dabe98b7160bacf1c2
Bernard Butaye <renardeau@users.noreply.github.com>
2024-02-25 18:25:25 +0100

    convert Tool from Model to simple class

--- src/classes/Model/Tool.php
+++ src/classes/Model/Tool.php
@@ -5,22 +5,22 @@ use Database\Factories\ToolFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tool extends Model
+class Tool // extends Model
 {
-    use HasFactory;
+    //use HasFactory;
 
-    /**
-     * Create a new factory instance for the model.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    protected static function newFactory()
-    {
-        return ToolFactory::new();
-    }
+    // /**
+    //  * Create a new factory instance for the model.
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Factories\Factory
+    //  */
+    // protected static function newFactory()
+    // {
+    //     return ToolFactory::new();
+    // }
 
-    protected $table = 'kb_tools';
-    protected $primaryKey = "tool_id";
+    //protected $table = 'kb_tools';
+    //protected $primaryKey = "tool_id";
 	static protected $fieldArray = ['tool_id', 'name', 'description', 'category', 'img', 'created_at', 'updated_at', 
 			'brand', 'type', 'serial', 'manufacturing_year', 'manufacturer_url', 'doc_url', 'code', 'owner_id', 'reception_date',
 			'state', 'visible', 'size', 'fee', 'deliverable'
@@ -37,14 +37,14 @@ class Tool extends Model
 		return $this->hasMany('Api\Model\Reservation', 'tool_id');
 	}
 
-    public function inventoryItem()
-    {
-        return $this->belongsTo('Api\Model\InventoryItem', 'tool_id');
-    }
+    // public function inventoryItem()
+    // {
+    //     return $this->belongsTo('Api\Model\InventoryItem', 'tool_id');
+    // }
     // Query helpers
-    public function scopeAll($query)
-    {
-        return $query->where('state', '<>', ToolState::DISPOSED);
-    }
+    // public function scopeAll($query)
+    // {
+    //     return $query->where('state', '<>', ToolState::DISPOSED);
+    // }
 
 }
