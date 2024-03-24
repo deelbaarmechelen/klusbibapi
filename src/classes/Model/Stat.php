0c621d2aaab17ecb822e813d1316dd3bb50c2a12
Bernard Butaye <renardeau@users.noreply.github.com>
2024-03-24 23:50:20 +0100

    phpstan: set baseline

--- src/classes/Model/Stat.php
+++ src/classes/Model/Stat.php
@@ -4,6 +4,12 @@ namespace Api\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static Builder where(...$params)
+ * @method static Model|\Illuminate\Database\Eloquent\Collection|null find($id, $columns = ['*'])
+ * @method static Model|\Illuminate\Database\Eloquent\Collection findOrFail($id, $columns = ['*'])
+ * @method static Model firstOrCreate(array $attributes = [], array $values = [])
+ */
 class Stat extends Model
 {
     protected $table = 'kb_stats';
