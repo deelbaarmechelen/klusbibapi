12db10e90859da9b24b1d5015b961b8fad793266
Bernard Butaye <renardeau@users.noreply.github.com>
2024-04-20 17:55:01 +0200

    avoid conflicting payment id between fee and payment

--- db/migrations/20240323185245_align_lendengine_payment.php
+++ db/migrations/20240323185245_align_lendengine_payment.php
@@ -40,7 +40,7 @@ class AlignLendenginePayment extends AbstractCapsuleMigration
         Capsule::update('INSERT INTO payment_method (id, name, is_active) VALUES (4, "Payconiq", 1),(5, "LETS", 1),(6, "Mechelen Bon (MBON)", 1),(7, "Kdo Bon (KDOBON)", 1),(8, "Other", 1)');
         Capsule::update('DELETE FROM payment WHERE id IN (SELECT payment_id FROM kb_payments)');
         Capsule::update('INSERT INTO payment (id, created_at, type, payment_date, amount, psp_code, note, contact_id, membership_id, loan_id, kb_payment_timestamp, kb_mode, kb_state, kb_order_id, kb_expiration_date) '
-            . 'SELECT payment_id, created_at, \'PAYMENT\', DATE(payment_date), amount, order_id, comment, user_id, membership_id, loan_id, payment_date, mode, state, order_id, expiration_date FROM kb_payments');
+            . 'SELECT (payment_id *2) -1, created_at, \'PAYMENT\', DATE(payment_date), amount, order_id, comment, user_id, membership_id, loan_id, payment_date, mode, state, order_id, expiration_date FROM kb_payments');
         Capsule::update('UPDATE payment SET payment_method_id = 1 WHERE UPPER(kb_mode) = \'CASH\'');
         Capsule::update('UPDATE payment SET payment_method_id = 2 WHERE UPPER(kb_mode) = \'MOLLIE\'');
         Capsule::update('UPDATE payment SET payment_method_id = 3 WHERE UPPER(kb_mode) = \'TRANSFER\'');
@@ -51,7 +51,7 @@ class AlignLendenginePayment extends AbstractCapsuleMigration
         Capsule::update('UPDATE payment SET payment_method_id = 8 WHERE UPPER(kb_mode) IN (\'STROOM\', \'SPONSORING\', \'UNKNOWN\', \'OVAM\', \'OTHER\')');
         // insert membership fees
         Capsule::update('INSERT INTO payment (id, created_at, type, payment_date, amount, note, contact_id, membership_id, kb_payment_timestamp) '
-            . 'SELECT payment_id, created_at, \'FEE\', DATE(payment_date), amount, \'Membership fee.\', user_id, membership_id, payment_date FROM kb_payments WHERE NOT membership_id IS NULL');
+            . 'SELECT payment_id * 2, created_at, \'FEE\', DATE(payment_date), amount, \'Membership fee.\', user_id, membership_id, payment_date FROM kb_payments WHERE NOT membership_id IS NULL');
     }
     /**
      * Down Method.
